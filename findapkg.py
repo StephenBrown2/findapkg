f30498e37cba6a6a042bfd09babd4e8144a135ad
Stephen Brown II <Stephen.Brown2@gmail.com>
2017-01-05 11:24:09 -0600

    Python 3 changes and aur4 is now live

--- findapkg.py
+++ findapkg.py
@@ -27,23 +27,23 @@ def col_print(array,highlight=False,hlcol=0):
       # If the index is 1 (i.e. we're about to print the second row),
       # print a dividing line
       if i == 1:
-          print " | ".join("-" * width for val, width in zip(row,widths))
+          print(" | ".join("-" * width for val, width in zip(row,widths)))
       # If we've got a string to match and the column specified matches,
       # make it green
       if highlight != False and row[hlcol] == highlight:
           sys.stdout.write('\033[92m')
-          print " | ".join((val[:width].ljust(width) for val, width in zip(row,widths)))
+          print(" | ".join((val[:width].ljust(width) for val, width in zip(row,widths))))
           sys.stdout.write('\033[0m')
       # Otherwise, continue on
       else:
-          print " | ".join((val[:width].ljust(width) for val, width in zip(row,widths)))
+          print(" | ".join((val[:width].ljust(width) for val, width in zip(row,widths))))
     # At the very end, print a solid line of dashes and a newline
-    print "---".join("-" * width for width in widths)
-    print
+    print("---".join("-" * width for width in widths))
+    print()
 
 for pkg in pkgsearch:
     pkgs = [['Repository','Package Name','Arch','Version','Description']]
-    print "Searching for {} in Arch repositories and the AUR".format(pkg)
+    print("Searching for {} in Arch repositories and the AUR".format(pkg))
     # Grab the Arch Repo results
     r = requests.get("https://www.archlinux.org/packages/search/json/?q={}".format(pkg))
     ar = r.json()['results']
@@ -52,7 +52,7 @@ for pkg in pkgsearch:
         pkgs.append([res['repo'],res['pkgname'],res['arch'],res['pkgver']+"-"+res['pkgrel'],res['pkgdesc']])
 
     # Grab the AUR results
-    u = requests.get("https://aur4.archlinux.org/rpc.php?type=search&arg={}".format(pkg))
+    u = requests.get("https://aur.archlinux.org/rpc.php?type=search&arg={}".format(pkg))
     au = u.json()['results']
     # And append to the array
     for res in au:
@@ -62,4 +62,4 @@ for pkg in pkgsearch:
     if len(pkgs) > 1:
         col_print(pkgs,highlight=pkg,hlcol=1)
     else:
-        print "No results found!"
+        print("No results found!")
